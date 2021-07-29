76299f7b38607183322cf4b48bd119093fa2be7d
krt-n201x <kitsada_t@cmu.ac.th>
2021-07-29 17:13:13 +0700

    1.6

--- src/views/event/Edit.vue
+++ src/views/event/Edit.vue
@@ -1,9 +1,26 @@
 <template>
   <p>Edit the event here</p>
+  <button @click="edit">edit Me</button>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  inject: ['GStore'], //<---- Inject the Global Store
+  methods: {
+    edit() {
+      this.GStore.flashMessage =
+        'You are seccessfully edited for ' + this.event.title
+      setTimeout(() => {
+        //After 3 seconds remove it
+        this.GStore.flashMessage = ''
+      }, 3000)
+      //Set a flash message to appear on the next page loaded which says
+      this.$router.push({
+        name: 'EventDetails',
+        params: { id: this.event.id }
+      })
+    }
+  }
 }
 </script>
